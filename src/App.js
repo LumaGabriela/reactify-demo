9be6b79e8a040d207461522647a58b9bbfe0c8c8
Luma Gabriela <lumagabriela3331@gmail.com>
2025-01-16 13:54:31 -0300

    Update styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import Botao from './components/button'
-import * as React from 'react'
-import Alert from 'react-bootstrap/Alert';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import * as React from 'react'
+import NavBar from './components/navbar/navbar'
+import './App.css'
 
-function Example() {
-  return (
-    <Alert dismissible variant="danger">
-      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>Change this and that and try again.</p>
-    </Alert>
-  );
-}
 function App() {
   return (
     <div className="App">
-      <Botao/>
-      <Example/>
+      <NavBar/>
+      
+
     </div>
   );
 }
